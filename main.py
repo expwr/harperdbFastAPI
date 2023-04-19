8070a347e42c3e1df79f8085eb7b52d13cd73d26
expisexperience <noah.diana10312005@gmail.com>
2023-04-19 11:50:48 -0400

    db connect attempt v1

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from database import db
 
 app = FastAPI()
 
 @app.get("/")
 def getRoutes():
     return ['/notes', '/notes/<pk>']
+
+@app.get("/notes")
+def getRoutes():
+    notes = db.search_by_value('notesapp', 'notes', 'id', '*', get_attributes=['*'])
+    return notes
---
+++ secrets.py
@@ -0,0 +1,3 @@
+HARPERDB_PASSWORD = 'Noey10312005!'
+HARPERDB_URL = 'https://notes-expwr.harperdbcloud.com'
+HARPERDB_USERNAME = 'expwr'
